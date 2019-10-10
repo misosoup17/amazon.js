c8ce35d7dd3e4d6d48684fbd1df4f3d29b6979e1
misosoup17 <misagaspar@icloud.com>
2019-10-09 20:22:15 -0700

    updated

--- BamazonCustom.js
+++ BamazonCustom.js
@@ -1,66 +1,147 @@
-var mysql = require('mysql');
-var prompt = require('prompt');
+// Pull in required dependencies
 var inquirer = require('inquirer');
+var mysql = require('mysql');
 
+// Define the MySQL connection parameters
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Smisa5932475',
-    database: 'bamazon'
+	host: 'localhost',
+	port: 3306,
+
+	// Your username
+	user: 'root',
+
+	// Your password
+	password: 'Smisa5932475',
+	database: 'Bamazon'
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-});
+// validateInput makes sure that the user is supplying only positive integers for their inputs
+function validateInput(value) {
+	var integer = Number.isInteger(parseFloat(value));
+	var sign = Math.sign(value);
 
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return 'Please enter a whole non-zero number.';
+	}
+}
+
+// promptUserPurchase will prompt the user for the item/quantity they would like to purchase
+function promptUserPurchase() {
+	// console.log('___ENTER promptUserPurchase___');
+
+	// Prompt the user to select an item
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'item_id',
+			message: 'Please enter the Item ID which you would like to purchase.',
+			validate: validateInput,
+			filter: Number
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many do you need?',
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
+		// console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
+
+		var item = input.item_id;
+		var quantity = input.quantity;
+
+		// Query db to confirm that the given item ID exists in the desired quantity
+		var queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
+
+			// If the user has selected an invalid item ID, data attay will be empty
+			// console.log('data = ' + JSON.stringify(data));
+
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+				displayInventory();
+
+			} else {
+				var productData = data[0];
+
+				// console.log('productData = ' + JSON.stringify(productData));
+				// console.log('productData.stock_quantity = ' + productData.stock_quantity);
+
+				// If the quantity requested by the user is in stock
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+					// Construct the updating query string
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+					// console.log('updateQueryStr = ' + updateQueryStr);
+
+					// Update the inventory
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with us!');
+						console.log("\n---------------------------------------------------------------------\n");
+
+						// End the database connection
+						connection.end();
+					})
+				} else {
+					console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+					console.log('Please modify your order.');
+					console.log("\n---------------------------------------------------------------------\n");
+
+					displayInventory();
+				}
+			}
+		})
+	})
+}
+
+// displayInventory will retrieve the current inventory from the database and output it to the console
 function displayInventory() {
-	connection.query('SELECT * FROM Products', function(err, res) {
-        if (err) throw err;
-        console.log("====================");
-        console.log("Bamazon Inventory");
-        console.log("====================");
-        for (var i = 0; i < res.length; i++) {
-            console.log("Item ID: " + res[i].ItemID +
-            " | Product: " + res[i].ProductName +
-            " | Department: " + res[i].DepartmentName +
-            " | Price: $" + res[i].Price +
-            " | Quantity: " + res[i].StockQuantity);
-        }
+	// console.log('___ENTER displayInventory___');
 
-        inquirer.prompt([
-        
-        	{
-        		type: 'input',
-        	    message: 'Which item would you like to purchase? Enter Item ID.',
-        	    name: 'id'
-        	},
-        	{
-        		type: 'input',
-        	    message: 'How many would you like to purchase?',
-        	    name: 'quantity'
-        	}
+	// Construct the db query string
+	queryStr = 'SELECT * FROM products';
 
-        ]).then(function(order) {
-        	var quantity = order.quantity;
-        	var itemId = order.id;
-        	connection.query('SELECT * FROM Products WHERE ItemID=' + itemId, function(err, selectedItem) {
-        		if (err) throw err;
-        		if (selectedItem[0].StockQuantity - quantity >= 0) {
-        			console.log("Quantity in Stock: " + selectedItem[0].StockQuantity + " Order Quantity: " + quantity);
-                    console.log("You will be charged $" + (order.quantity * selectedItem[0].Price) + ". Thank you for shopping at Bamazon. Come back soon!");
-                    connection.query('UPDATE Products SET StockQuantity=? WHERE ItemID=?', [selectedItem[0].StockQuantity - quantity, itemId], function(err, inventory) {
-                    	if (err) throw err;
-                    	displayInventory();
-                    });
-        		}
-        		else {
-        			console.log("Insufficient quantity. Please enter a valid amount.");
-        			displayInventory();
-        		};
-        	});
-        });
-    });
-};
+	// Make the db query
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
 
-displayInventory();
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+
+		var strOut = '';
+		for (var i = 0; i < data.length; i++) {
+			strOut = '';
+			strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+			strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+			strOut += 'Department: ' + data[i].department_name + '  //  ';
+			strOut += 'Price: $' + data[i].price + '\n';
+
+			console.log(strOut);
+		}
+
+	  	console.log("---------------------------------------------------------------------\n");
+
+	  	//Prompt the user for item/quantity they would like to purchase
+	  	promptUserPurchase();
+	})
+}
+
+// runBamazon will execute the main application logic
+function runBamazon() {
+	// console.log('___ENTER runBamazon___');
+
+	// Display the available inventory
+	displayInventory();
+}
+
+// Run the application logic
+runBamazon();
--- schemma.sql
+++ schemma.sql
@@ -2,45 +2,39 @@
 DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 
-USE bamazon;
+CREATE DATABASE Bamazon;
+USE Bamazon;
 
-CREATE TABLE products(
-  item_id INT AUTO_INCREMENT NOT NULL,
-  product_name VARCHAR(45) NOT NULL,
-  product_sales DECIMAL(10,2) DEFAULT 0,
-  department_name VARCHAR(45) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  stock_quantity INT(10) NOT NULL,
-  primary key(item_id)
+-- Create a table called 'products' which will contain the store inventory --
+
+CREATE TABLE products (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY item_id
 );
 
-SELECT * FROM products;
-
+-- Insert data into the 'products' table --
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Uncharted 4", "Video Games", 49.95, 150),
-  ("DOOM", "Video Games", 59.99, 200),
-  ("Crate of Spam", "Food and Drink", 24.50, 50),
-  ("Cool Shades", "Apparel", 75.00, 5),
-  ("Worn Denim Jeans", "Apparel", 54.25, 35),
-  ("Survival Towel", "Necessities", 42.42, 42),
-  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
-  ("Mad Max: Fury Road", "Films", 25.50, 57),
-  ("Monopoly", "Board Games", 30.50, 35),
-  ("Yahtzee", "Board Games", 19.95, 23);
-
-  CREATE TABLE departments(
-  department_id INT AUTO_INCREMENT NOT NULL,
-  department_name VARCHAR(45) NOT NULL,
-  over_head_costs DECIMAL(10,2) NOT NULL,
-  primary key(department_id)
-);
-
-SELECT * FROM departments;
-
-INSERT INTO departments (department_name, over_head_costs)
-VALUES ("Video Games", 200),
-  ("Food and Drink", 100),
-  ("Apparel", 50),
-  ("Necessities", 300),
-  ("Films", 35),
-  ("Board Games", 0);
+VALUES  ('Dove Shampoo', 'Cosmetics', 5.75, 500),
+		('Dove Conditioner', 'Cosmetics', 6.25, 627),
+		('Glad 12 Gal Trash Bags', 'Grocery', 5.99, 300),
+		('Brawny Paper Towels', 'Grocery', 4.25, 400),
+		('Granny Smith Apples', 'Produce', 0.35, 800),
+		('Chiquita Bannana', 'Produce', 0.20, 10000),
+		('Tropicana Orange Juice', 'Grocery', 4.45, 267),
+		('Horizon Organic Milk', 'Grocery', 4.50, 200),
+		('Huggies Diapers', 'Children', 2.75, 476),
+		('Charmin Toiler Paper', 'Grocery', 12.99, 575),
+		('Pampers Baby Wipes', 'Children', 1.50, 423),
+		('Yoga Mat', 'Sports', 12.75, 150),
+		('5lb Dumb bell', 'Sports', 7.99, 89),
+		('Tie Dye Shirt', 'Clothing', 5.55, 120),
+		('Nike Shorts', 'Clothing', 17.88, 250),
+		('Purina Cat Chow', 'Pet', 7.25, 157),
+		('Fancy Feast Wet Cat Food', 'Pet', 12.50, 163),
+		('Ibuprophen', 'Pharmacy', 4.95, 389),
+		('Band Aid', 'Pharmacy', 3.25, 550),
+		('Ben & Jerry Ice Cream', 'Grocery', 3.25, 432);
